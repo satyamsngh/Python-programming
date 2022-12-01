8c03d5064a67f52a57ebe8d76b51f74aa1598558
Satyam Singh <101830202+satyamsngh@users.noreply.github.com>
2022-12-01 19:15:13 +0530

    adding problems

--- pythonProblems/digonal_pattern.py
+++ pythonProblems/digonal_pattern.py
@@ -1,7 +1,7 @@
 n=7
-for i in range(0,n):
+for i in range(n):
     
-    for j in range(0,n):
+    for j in range(n):
         if (i==0 or j==0 or i==j or i==n-1 or j==n-1 or i+j==n-1):
                 print("*",end=" ")
         else:
