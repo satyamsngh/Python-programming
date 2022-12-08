923e663eb50275eb00f0da330e3359764cb3d123
Satyam Singh <101830202+satyamsngh@users.noreply.github.com>
2022-12-08 23:56:14 +0530

    add

--- pythonProblems/vector_product_of_twoarray.py
+++ pythonProblems/vector_product_of_twoarray.py
@@ -1,11 +1,12 @@
 arr1 = [1, 2, 6, 3, 7]
 arr2 = [10, 7, 45, 3, 7]
 n=len(arr1)
+#sorting an first array
 for i in range(n):
     for j in range(i+1,n):
         if arr1[i]>arr1[j]:
             arr1[i],arr1[j]=arr1[j],arr1[i]
-
+#sorting second array
 for i in range(n):
     for j in range(i+1,n):
         if arr2[i]<arr2[j]:
