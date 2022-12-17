932ff3332838e1c220df3f03f604bc203edc28f9
Satyam Singh <101830202+satyamsngh@users.noreply.github.com>
2022-12-17 15:33:23 +0530

    add

---
+++ LinkedList/create_linkedlist.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class linkedlist:
+    def __init__(self):
+        self.head=None
+    def printlist(self):
+        temp=self.head
+        while(temp):
+            print(temp.data)
+            temp=temp.next
+list=linkedlist()
+list.head=Node(2)
+second=Node(3)
+third=Node(4)
+list.head.next=second
+second.next=third
+list.printlist()                        
