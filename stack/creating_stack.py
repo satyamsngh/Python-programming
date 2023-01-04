bc14da0ad6e75466972943892b71b99c38adedda
Satyam Singh <101830202+satyamsngh@users.noreply.github.com>
2023-01-05 01:20:17 +0530

    solving

---
+++ stack/creating_stack.py
@@ -0,0 +1,22 @@
+stack=[]
+#adding value in the stack
+stack.append("a")
+stack.append("b")
+stack.append("c")
+stack.append("d")
+stack.append("e")
+#printing the stack
+print(stack)
+
+#deleting the value in the stack
+stack.pop()
+print(stack)
+stack.pop()
+print(stack)
+stack.pop()
+print(stack)
+stack.pop()
+print(stack)
+stack.pop()
+#stack is empty
+print(stack)
